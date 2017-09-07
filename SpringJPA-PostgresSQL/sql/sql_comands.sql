ce4678141b6ba57be95cf2d4d875919ee85351bf
Jaspal Dhaliwal <jasdhali@gmail.com>
2017-09-06 22:13:26 -0500

    Required SQL Commands

---
+++ SpringJPA-PostgresSQL/sql/sql_comands.sql
@@ -0,0 +1,6 @@
+create sequence hibernate_sequence increment 1 start 1;
+
+createdb --username=postgres --password=postgres testdb
+
+http://javasampleapproach.com/spring-framework/use-spring-jpa-postgresql-spring-boot
+
